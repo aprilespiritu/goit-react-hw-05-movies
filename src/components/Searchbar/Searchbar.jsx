d32c9fa4d76b903042e9c5898d39a1d6d20c6309
aprilespiritu <april_espiritu@live.com>
2024-08-05 22:05:32 +0800

    Update Searchbar.jsx

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,9 @@
+import { SearchForm } from "./SearchForm";
+import { SearchInput } from "./SearchInput";
+import PropTypes from 'prop-types';
+import css from './Searchbar.module.css';
+import { BsSearchHeart } from 'react-icons/bs';
+
+export const Searchbar = ({ onSubmit }) => (
+    <Search
+)
