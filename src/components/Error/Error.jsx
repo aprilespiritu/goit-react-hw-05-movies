763ceea66a6d3a73b663a9a1b880c4ff4b0fee90
aprilespiritu <april_espiritu@live.com>
2024-08-19 22:34:41 +0800

    Error

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,12 @@
+import css from './Error.module.css';
+
+const Error = ({ message }) => {
+    return (
+        <div className={css.error}>
+            <p>Oops! Something went wrong. Please try again.</p>
+            <p>Error: {message}</p>
+        </div>
+    );
+};
+
+export default Error;
