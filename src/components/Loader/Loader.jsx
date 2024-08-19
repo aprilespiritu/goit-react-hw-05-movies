807486ed0739c7411b763ed592cec1386a7b085a
aprilespiritu <april_espiritu@live.com>
2024-08-19 22:09:22 +0800

    Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,19 @@
+import { DNA } from "react-loader-spinner";
+import css from './Loader.module.css';
+
+const Loader = () => {
+    return (
+        <div>
+            <DNA
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="dna-loading"
+                wrapperStyle={{}}
+                wrapperClass="dna-wrapper"
+            />
+        </div>
+    );
+};
+
+export default Loader;
