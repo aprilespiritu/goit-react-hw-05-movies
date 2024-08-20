47d5029f1a312ae45c11710c468fab5a1cc70c21
aprilespiritu <april_espiritu@live.com>
2024-08-21 03:19:54 +0800

    Update ReviewsPage.jsx

--- src/pages/ReviewsPage/ReviewsPage.jsx
+++ src/pages/ReviewsPage/ReviewsPage.jsx
@@ -6,10 +6,36 @@ import Error from "components/Error/Error";
 import Reviews from "components/Reviews/Reviews";
 
 const ReviewsPage = () => {
-    const [reviews, getReviews] = useState(null);
+    const [reviews, setReviews] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, settError] = useState(null);
     const { movieId } = useParams();
 
-    
-}
+    useEffect(() => {
+        if (!movieId) return;
+        const getMovies = async () => {
+            try {
+                setLoading(true);
+                settError(null);
+                const data = await getMovieReviews(movieId);
+                setReviews(data.results);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getMovies();
+    }, [movieId]);
+
+    return (
+        <section>
+            {loading && <Loader />}
+            {reviews !== null && <Reviews reviews={reviews} />}
+            {error !== null && <Error message={error.message} />}
+        </section>
+    );
+};
+
+export default ReviewsPage;
