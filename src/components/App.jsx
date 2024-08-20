343ac3d987f1627cc349129cee2b5efa1dfd8183
aprilespiritu <april_espiritu@live.com>
2024-08-21 03:32:03 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
+import { Link, Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+import Header from './Header/Header';
+import MovieDetailsPage from 'pages/MovieDetailsPage/MovieDetailsPage';
+import CastPage from 'pages/CastPage/CastPage';
+import ReviewsPage from 'pages/ReviewsPage/ReviewsPage';
+import Movies from 'pages/Movies/Movies';
+
+import Home = lazy(() => import('pages/Home/Home'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<ReviewsPage />} />
+            <Route path="reviews" element={<ReviewsPage />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <section className="container">
+                <Link className="go-to-home" to="/">
+                  Go to homepage
+                </Link>
+              </section>
+            }
+          />
+        </Route>
+      </Routes>
+    </>
   );
 };
+
+export default App;
